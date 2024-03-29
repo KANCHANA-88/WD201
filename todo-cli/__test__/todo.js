340a22f1d528960406f0df1351f146f5a9545cdf
KANCHANA-88 <105222558+KANCHANA-88@users.noreply.github.com>
2024-03-29 14:35:20 +0530

    Add files via upload

---
+++ todo-cli/__test__/todo.js
@@ -0,0 +1,24 @@
+const todoList = require('../todo');
+
+
+const {all,markAsComplete,add} = todoList();
+
+describe("Todolist Test Suite", () => {
+
+    test("should add new todo", ()=> {
+        expect(all.length).toBe(0);
+        add({
+            title: "Test todo",
+            completed: false,
+            dueDate:new Date().toLocaleDataString("en-CA")
+        }
+        );
+        expect(all.length).toBe(1);
+    });
+
+    test("should mark a todo as complete", () => {
+        expect(all[0].completed).toBe(false);
+        markAsComplete(0);
+        expect(all[0].completed).toBe(true);
+    })
+})
