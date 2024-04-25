fd52d55e54731a0451f4c96e62b2fce7088098f4
KANCHANA-88 <105222558+KANCHANA-88@users.noreply.github.com>
2024-04-25 21:28:04 +0530

    Update index.js

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,9 +1,5 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000; // Use the port provided by the environment or default to 3000
+const app = require('./app')
 
-// Define your routes and middleware here
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(3000, () =>{
+    console.log("started expree server at port 3000")
+})
