bacc59fdb0ad012b9dcd0abc88c7b8afb6c83fed
KANCHANA-88 <105222558+KANCHANA-88@users.noreply.github.com>
2024-04-25 21:42:54 +0530

    Update index.js

--- todo-app/index.js
+++ todo-app/index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000; // Use the port provided by the environment or default to 3000
 
-// Define route to handle requests to the root URL
-app.get('/', (req, res) => {
-  res.send('Hello, World!'); // Respond with a simple message
-});
+const app = require("./app");
+
+
 
 // Start the server
 app.listen(port, () => {
