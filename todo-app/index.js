c2990b04790468bb19698a086a8196a8fba04778
KANCHANA-88 <105222558+KANCHANA-88@users.noreply.github.com>
2024-04-25 21:46:05 +0530

    Update index.js

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,12 +1,5 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000; // Use the port provided by the environment or default to 3000
-
 const app = require("./app");
 
-
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(3000, () => {
+  console.log("Server started at port 3000!");
 });
