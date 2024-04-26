34d54c5c6bc99fd71ac697a8586b17d9b8604090
KANCHANA-88 <105222558+KANCHANA-88@users.noreply.github.com>
2024-04-26 17:46:29 +0530

    Commit message

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,5 +1,13 @@
-const app = require("./app");
+const express = require('express')
+
+const app = express()
+
+
+app.get("/", (request, response) => {
+    response.send('hello world')
+})
+
 
 app.listen(3000, () => {
-  console.log("Server started at port 3000!");
-});
+    console.log("started express server at port 3000")
+})
